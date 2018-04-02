da38cb3bcdc8a14446e13236b04f1ad569b3d467
telenz <teresa.lenz@desy.de>
2018-04-02 21:02:03 +0200

    write final party location to different sheet in same excel file + rescueTableProbability added

--- tmp/randomDinnerGeneratorTest.py
+++ tmp/randomDinnerGeneratorTest.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import sys
 sys.path.insert(0,'../')
 import src.randomDinnerGenerator as rdg
@@ -8,15 +9,20 @@ Dinner1 = rdg.randomDinnerGenerator(numberOfTeams=10
                                     ,wishStarterProbability=0.7
                                     ,wishMainCourseProbability=0.05
                                     ,wishDessertProbability=0.2
+                                    ,rescueTableProbability=0.1
                                     ,meatIntolerantProbability=0.3
                                     ,animalProductsIntolerantProbability=0.1
                                     ,lactoseIntolerantProbability=0.02
                                     ,fishIntolerantProbability=0.1
                                     ,seafoodIntolerantProbability=0.1
                                     ,dogsIntolerantProbability=0.05
-                                    ,catsIntolerantProbability=0.05
+                                    ,catsIntolerantProbability=0.2
                                     ,dogFreeProbability=0.9
                                     ,catFreeProbability=0.7
-                                    ,verbose=0)
-dinner=Dinner1.generateDinner()
-dinner.to_excel("dinnerTest.xlsx")
+                                    ,verbose=1)
+dinner,finalPartyLocation=Dinner1.generateDinner()
+print finalPartyLocation
+excel_writer = pd.ExcelWriter('dinnerTest.xlsx')
+dinner.to_excel(excel_writer,'teams')
+finalPartyLocation.to_excel(excel_writer,'final_party_location',index=None, header=False)
+excel_writer.save()

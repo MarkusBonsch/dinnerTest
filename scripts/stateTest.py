9eefcfc858d902e510d1b9c65f5fb0e9d7f81c0f
MarkusBonsch <mbonsch@posteo.de>
2024-10-27 10:52:47 +0100

    Bugfixes in stateTest.py, related to python 3 adaptation and others.

--- scripts/stateTest.py
+++ scripts/stateTest.py
@@ -1,35 +1,34 @@
 import pandas as pd
 import sys
 from datetime import datetime
-sys.path.insert(0,'../')
+sys.path.insert(0,'C:/users/markus_2/Documents/Nerding/python/dinnerTest')
+sys.path.insert(0,'C:/users/markus_2/Documents/Nerding/python/dinnerTest/src')
 from src.state import state
 from src.randomAgent import randomAgent
+import assignDinnerCourses as adc
 import pdb
 
 excel_file = pd.ExcelFile('test.xls')
 #finalDinnerLocation=xl.sheet_names[0]
-dinner = pd.read_excel(excel_file)
-
+dinner = pd.read_excel(excel_file, sheet_name = "teams")
+finalPartyLocation = pd.read_excel(excel_file, sheet_name = "final_party_location", )
 dinnerTime = datetime(2018, 7, 1, 20)
 
-myState = state(data = dinner, dinnerTime = dinnerTime)
+courseAssigner = adc.assignDinnerCourses(dinnerTable = dinner)
+dinnerAssigned = courseAssigner.assignDinnerCourses(random = False)
+
+myState = state(data = dinnerAssigned, finalPartyLocation = finalPartyLocation, dinnerTime = dinnerTime, travelMode="simple")
 
 myState.initNormalState()
 
-myAgent = randomAgent(myState)
+myAgent = randomAgent()
 
 i = 1
 while not myState.isDone:
-    print 'activeCourse = ' + str(myState.activeCourse)
-    print 'activeTeam = ' + str(myState.activeTeam)
-    print i
+    print('activeCourse = ' + str(myState.activeCourse))
+    print('activeTeam = ' + str(myState.activeTeam))
+    print(i)
 #    pdb.set_trace()
-    if i == 18:
-#        pdb.set_trace()
-        pass
-    if myState.activeTeam == 3:
-#        pdb.set_trace()
-        pass
     action = myAgent.chooseAction(myState, random=False)
     myState.update(action)
     i+=1
